8ed088d52500066dbb6153b116794dbacedc7991
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-03-26 13:32:32 +0100

    Added models - users

---
+++ APBD_C02_s33507/Models/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_C02_s33507.Models;
+
+public class Employee(string fName, string lName) : User(fName, lName)
+{
+    public override string GetUserType() => "Employee";
+}
---
+++ APBD_C02_s33507/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_C02_s33507.Models;
+
+public abstract class User(string fName, string lName)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; set; } = _nextId++;
+    public string FName { get; set; } = fName;
+    public string LName { get; set; } = lName;
+    public abstract string GetUserType();
+
+}

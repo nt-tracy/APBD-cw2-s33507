35815b1440bced6ad88dffb22d5a2e0df9248c51
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-03-26 17:05:39 +0100

    Added ability to rent

---
+++ APBD_C02_s33507/Models/EquipmentRental.cs
@@ -0,0 +1,15 @@
+﻿namespace APBD_C02_s33507.Models;
+
+public class EquipmentRental(EquipmentType equipmentType, User user, DateTime from, DateTime to, bool returned)
+{
+    private static int _nextId = 1;
+    public int  Id { get; set; } =  ++_nextId;
+    
+    public EquipmentType EquipmentType { get; set; }
+    public User User { get; set; }
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+    public bool Returned { get; set; } = false;
+    
+    
+}

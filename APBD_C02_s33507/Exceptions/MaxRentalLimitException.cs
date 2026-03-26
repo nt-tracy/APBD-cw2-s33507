709283163a4331b5e5cb725339ad43e9b9223b1c
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-03-26 21:56:20 +0100

    Added Exceptions

---
+++ APBD_C02_s33507/Exceptions/MaxRentalLimitException.cs
@@ -0,0 +1,8 @@
+﻿using APBD_C02_s33507.Models;
+
+namespace APBD_C02_s33507.Exceptions;
+
+public class MaxRentalLimitException(User user) : Exception($"Użytkownik {user.LName} przekroczył limit {user.MaxRentals} wypożyczeń.")
+{
+
+}
---
+++ APBD_C02_s33507/Exceptions/UnavailableEquipmentException.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_C02_s33507.Exceptions;
+
+public class UnavailableEquipmentException(int equipmentId) : Exception($"Equipment with id {equipmentId} is not available.")
+{
+    
+}
